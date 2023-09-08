b647883ac8d1dfcf8fa53273d337f35eeface27d
mswitas <mswitas@gmail.com>
2023-09-08 12:07:46 +0200

    add button and action

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textview;
+    Button button;
+    String name = "Maciej Świtas";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textview.setText(name);
+            }
+        });
     }
 }
